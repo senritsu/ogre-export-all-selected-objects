5b66299d269456d6a39ca1efb2ec1209efbe5f98
trichards <trichards@localhost>
2010-10-04 13:00:00 -0400

    Reorganized imports as suggested by jonim8or. Uncommented exporting normals.

--- ogreio/__init__.py
+++ ogreio/__init__.py
@@ -1,17 +1,22 @@
 """OGRE Mesh and Scene Exporters
 
    @author Tony Richards
+
+    Additional contributors and testers:
+        jonim8or
 """
 
-try:
+if "init_data" in locals():
     init_data
-    reload(export_mesh)
     reload(ogre_mesh)
-    reload(ogre_test)
-except:
-    from ogreio import export_mesh
+    reload(export_mesh)
+else:
+    import bpyml
+    bpyml.tag_module("ogrexml", ("mesh", "submeshes", "submesh", "submeshnames", 
+        "submeshname", "faces", "face", "geometry", "vertexbuffer", "vertex",
+        "position", "normal", "texcoord"))
     from ogreio import ogre_mesh
-    from ogreio import ogre_test
+    from ogreio import export_mesh
 
 def register():
     export_mesh.register()
